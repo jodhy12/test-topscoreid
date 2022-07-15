e9406cf6457f68744fcd5f37188865a66553ae86
jodhy12 <jodhy12@aol.com>
2022-07-15 11:08:43 +0700

    Test Topscoreid

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -10,33 +10,39 @@ use Illuminate\Support\Facades\Http;
 
 class KaryawanController extends Controller
 {
+
+    // GET Home Page
     public function index()
     {
         $karyawan = $this->getUsers();
         return view('admin.karyawan', compact('karyawan'));
     }
 
+    // GET Absen Page
     public function kehadiran(Request $request)
     {
+        // Get endpoint query
         $filter = $request->query('total');
         $dataPerPage = $request->query('perPage');
-        if ($dataPerPage != 5 && $dataPerPage != 10 && $dataPerPage != 15) {
-            $dataPerPage = 5;
-        }
-        if ($filter !== 'all' && $filter !== 'absen') {
-            $filter = 'all';
-        }
+
+        // Validate
+        $dataPerPage = $dataPerPage != 5 && $dataPerPage != 10 && $dataPerPage != 15 ? 5 : $dataPerPage;
+        $filter = $filter !== 'absen' ? 'all' : 'absen';
+
+        // Process Data
         $absens = $this->dataUsers($filter);
         $absens = $this->paginator($absens, $dataPerPage, $request->page);
         return view('admin.kehadiran.kehadiran', compact('absens', 'filter', 'dataPerPage'));
     }
 
+    // GET Create Absen Page
     public function addKehadiran()
     {
         $users = $this->getUsers();
         return view('admin.kehadiran.addKehadiran', compact('users'));
     }
 
+    // POST Store Absen
     public function store(Request $request)
     {
         Http::post('https://ayohadir.id/v1/input_absen', [
@@ -44,6 +50,7 @@ class KaryawanController extends Controller
         ]);
     }
 
+    // Paginate Function
     public function paginator($data, $perPage = 5, $currentPage = null, $options = [])
     {
         $perPage = $perPage ?: (Paginator::resolveCurrentPage() ?: 1);
@@ -51,6 +58,7 @@ class KaryawanController extends Controller
         return new LengthAwarePaginator($data->forPage($currentPage, $perPage), $data->count(), $perPage, $currentPage, $options);
     }
 
+    // GET Api Absen
     public function getAbsen()
     {
         $response = Http::get('https://ayohadir.id/v1/absen');
@@ -58,6 +66,7 @@ class KaryawanController extends Controller
         return $absens->results;
     }
 
+    //GET Api Karyawan
     public function getUsers()
     {
         $response = Http::get('https://ayohadir.id/v1/karyawan');
@@ -65,12 +74,15 @@ class KaryawanController extends Controller
         return $users->results;
     }
 
+    //GET function all data absen
     public function dataAbsen()
     {
         $absensRaw = $this->getAbsen();
         return $absensRaw;
     }
 
+    //GET function all data user with absen relation
+    //the @params is for the filter data
     public function dataUsers($params)
     {
 
